11923b4b4a4199f363fcc93907e16b5f8f3ab7d6
Catunima <2009055@upy.edu.mx>
2021-06-07 16:53:42 -0500

    some coment

--- week05/ex02second.c
+++ week05/ex02second.c
@@ -3,12 +3,14 @@
 int main(void){
 
   int seg= 0,hr,min,sg;
-//test.
+//variable seg es la que guardará los segundos ingresados
   printf("Please, input your seconds: ");
   scanf("%d", &seg);
   hr = (seg/3600);
   min = (seg -(hr*3600))/60;
   sg = seg -(hr*3600+min*60);
+
+//estos if solo anexan un "O" antes del numero en caso que estos sean menor a 10 para dar un formato así: 01:01:01
 
   if(hr <10){
     printf("0%d:",hr);
